8bdfe7263e1d68f52e751b7ecb7a9d7051c60684
predeinnikita <predein697@gmail.com>
2024-02-04 14:19:10 +0500

    fix(main)

--- packages/core/src/domain/repositories/index.ts
+++ packages/core/src/domain/repositories/index.ts
@@ -1,5 +1,4 @@
 export * from './timer.repository';
-export * from './analytic.repository';
 export * from './navigation.repository';
 export * from './memory.repository';
 export * from './fps.repository';
