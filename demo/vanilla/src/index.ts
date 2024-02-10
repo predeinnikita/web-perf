bacf4640c1cca15b572250b37812bcddfc58d50d
predeinnikita <predein697@gmail.com>
2024-02-10 14:59:10 +0500

    feat(yandex)

--- demo/vanilla/src/index.ts
+++ demo/vanilla/src/index.ts
@@ -1,5 +1,12 @@
 import {WebPerf} from "../../../packages/core/src/web-perf";
+import {YandexMetricsService} from '../../../packages/yandex/src';
 
-const wepPerf = new WebPerf();
+
+const wepPerf = new WebPerf({
+    metricsService: new YandexMetricsService({
+        id: 96422324,
+    })
+});
 wepPerf.startMonitoring();
+// wepPerf.sendStats();
 
